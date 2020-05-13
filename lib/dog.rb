9efa6a58bf6014bdcb0583fd9ed3c78c6813bbce
Scott  Keplinger <skeplin17@gmail.com>
2020-05-13 17:14:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,11 @@
 class Dog
+
   def name=(dogs_name)
-    @this_is_dogs_name = dogs_name
+    @this_dogs_name = dogs_name
   end
 
   def name
-    @this_is_dogs_name
+    @this_dogs_name
   end
 end
 
